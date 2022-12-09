cf680fd063d4ea759db62d517890700b2c83b58f
Ng1n3 <code.with.muyiwa@gmail.com>
2022-12-08 16:18:32 -0800

    fixing

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ * Description: 'prints out a line of text'
+ * Return:  0
+ */
+
 int main(void)
 {
 	puts("Programming is like building a multilingual puzzle \n");
