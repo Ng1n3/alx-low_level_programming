f499c74f3b83f753ac1e3da04eb1dbafbbe3a82b
Ng1n3 <code.with.muyiwa@gmail.com>
2022-12-08 18:20:28 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
-
 /**
- * main - Entry point
- * Description: 'prints out a line of text'
- * Return:  0
+ * Description: main - Prints out a string to stdout.
+ * Return: 0 if success.
  */
-
 int main(void)
 {
-	puts(" \"Programming is like building a multilingual puzzle\n");
-
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
