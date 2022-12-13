689b05d1b34baec9fc8980f7f87e74c7e4e3b87e
Ng1n3 <code.with.muyiwa@gmail.com>
2022-12-13 15:41:14 -0800

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,13 +2,13 @@
 /**
 * print_last_digit - prints the three digits of a number.
 * @n: digit to find three of
-*Return: the Last three digits
+*Return: the Last digits
 */
 
 int print_last_digit(int n)
 {
 	if (n < 0)
 		n = n * -1;
-	_putchar ((n % 1000) + '0');
-	return (n % 1000);
+	_putchar ((n % 10) + '0');
+	return (n % 10);
 }
