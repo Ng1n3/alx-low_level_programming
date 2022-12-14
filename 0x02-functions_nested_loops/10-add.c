be21522c1e0262ebe9d7540519a1e1a7f43f3adb
Ng1n3 <code.with.muyiwa@gmail.com>
2022-12-13 16:51:11 -0800

    two arguements add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.c"
+/**
+* add - adds two numbers togehter.
+* @i: the first number to be added.
+* @j: the second number to be added.
+* Return: the sum of two numbers.
+*/
+
+int add(int j, int i)
+{
+	return (j + i);
+}
