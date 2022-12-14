2e6bc34a131599b0ed26a6a29d4cea40e9c11e19
Ng1n3 <code.with.muyiwa@gmail.com>
2022-12-13 16:28:01 -0800

    24hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,7 +6,8 @@
 
 void jack_bauer(void)
 {
-	int h, m;	
+	int h, m
+;
 	for (h = 0; h < 24; h++)
 	{
 		for (m = 0; m < 60; m++)
@@ -14,7 +15,7 @@ void jack_bauer(void)
 			_putchar((h / 10) + '0');
 			_putchar((h % 10) + '0');
 			_putchar(':');
-			_putchar((m / 10) - '0');
+			_putchar((m / 10) + '0');
 			_putchar((m % 10) + '0');
 			_putchar('\n');
 		}
