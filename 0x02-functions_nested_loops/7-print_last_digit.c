1e2573555a770bfd8cfbf9f6fcd513e25169a53a
Ng1n3 <code.with.muyiwa@gmail.com>
2022-12-14 14:55:02 -0800

    fix

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-* print_last_digit - prints the three digits of a number.
+* print_last_digit - prints the last digit of a number.
 * @n: digit to find the last place of.
 *Return: the Last digits.
 */
