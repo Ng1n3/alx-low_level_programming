739e5e40afd5eae8fe4c44fbf6394c92158a8426
Ng1n3 <code.with.muyiwa@gmail.com>
2022-12-17 06:41:01 -0800

    except 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print numers 1 to 9 do not
+ * print 2 and 4.
+ * Return: nothing
+ */
+
+void print_most_numbers(void)
+{
+	int i = 0;
+
+	while ( i <= 9)
+	{
+		if( i != '2' && i != '4')
+			_putchar(i + '0');
+		++i;
+	}
+	_putchar('\n');
+}
+		
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,7 @@
 #ifndef main_h
 #define main_h
 
+void print_most_numbers(void);
 void print_numbers(void);
 int mul(int a, int b);
 int _isdigit(int c);
