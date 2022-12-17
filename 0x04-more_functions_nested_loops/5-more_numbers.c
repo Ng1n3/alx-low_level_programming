6e19041d4d364ba97914f782eb4bb1df33b75cd3
Ng1n3 <code.with.muyiwa@gmail.com>
2022-12-17 07:21:22 -0800

    fixes

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,12 +8,14 @@
 void more_numbers(void)
 {
 	int i = 0;
-	int j = 0;
+	i = j = 0;
 	while (i <= 9)
 	{
 		while (j <= 14)
 		{
-			_putchar(j + '0');
+			if ( j >= 10)
+				_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
 			++j;
 		}
 		_putchar('\n');
