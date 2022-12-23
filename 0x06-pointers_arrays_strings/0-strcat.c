910a6d45e046ed4408325148dccda796cd76673f
Ng1n3 <code.with.muyiwa@gmail.com>
2022-12-22 20:34:29 -0800

    first commit

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strcat - fills memory with a constant byte
+ * @s: pointer to memeroy area 's'
+ * @b: constant byte to fill
+ * @n: number of bytes to fill
+ * Return: pointer to memory area 's'
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	unsigned int i;
+	
+	for (i = 0; i < n; i++)
+		s[i] = b;
+	return (s);
+}
