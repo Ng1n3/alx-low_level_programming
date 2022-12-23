f9e05361c3fa5e2c9e1303e21e388c2f0fe74c39
Ng1n3 <code.with.muyiwa@gmail.com>
2022-12-22 20:50:53 -0800

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strncat- copies memory area
+ * @dest: pointer to destination memory area
+ * @src: pointer to source memory area
+ * @n: number of bytes to copy
+ *  Return: pointer to destination memory area
+ */
+
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+	
+	for(i = 0; i < n; i++)
+		dest[i] = src [i]'
+	return (dest);
+}
